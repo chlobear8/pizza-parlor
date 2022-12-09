7fa2bf54b47deb636358c93370211fdc9cc2b0e0
Chloe ONeil <chloesporri@yahoo.com>
2022-12-08 16:06:10 -0800

    BUGFIX try to fix toppingSelection function

--- js/scripts.js
+++ js/scripts.js
@@ -14,12 +14,20 @@ Pizza.prototype.sizeSelection = function() {
   }
 };
 
+//Pizza.prototype.toppingOptions = function() {
+  //let t1 = "pepperoni";
+//}
+
 Pizza.prototype.toppingSelection = function() {
-  const pepperoni = {name: "pepperoni"};
-  let toppings = {products: [pepperoni]};
-  for (let i = 0, i < toppings.length; i += 1) {
-    if (toppings[i] === 0) {
-      return toppingSelection.push(toppings[i].value);
-    }
+  let t1 = "pepperoni";
+  if (this.topping === t1) {
+    toppingSelection.push(this.topping);
   }
-};
+
+
+//   for (let i = 0, i < toppings.length; i += 1) {
+//     if (toppings[i] === 0) {
+//       return toppingSelection.push(toppings[i].value);
+//     }
+//   }
+// };
